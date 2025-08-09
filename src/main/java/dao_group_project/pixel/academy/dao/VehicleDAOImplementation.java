1c69a701689586cd18089a909efcd920ef0a230f
balanviorel <balanviorel2@gmail.com>
2025-08-09 11:11:28 +0300

    Add repositories to VehicleDAOImplementation class

--- src/main/java/dao_group_project/pixel/academy/dao/VehicleDAOImplementation.java
+++ src/main/java/dao_group_project/pixel/academy/dao/VehicleDAOImplementation.java
@@ -1,5 +1,12 @@
 package dao_group_project.pixel.academy.dao;
 
+import org.springframework.stereotype.Repository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Repository
 public class VehicleDAOImplementation implements VehicleDAO {
 
 
